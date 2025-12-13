502bfee7bb1b506f8ee92bf805f879eef48f4a2b
FrenskyOey <frenskyoey@gmail.com>
2025-12-13 21:23:33 +0700

    [Detail] initial layout for plan_screen

--- makanapa/lib/features/home/presentation/plan/screens/plan_screen.dart
+++ makanapa/lib/features/home/presentation/plan/screens/plan_screen.dart
@@ -9,6 +9,7 @@ import 'package:makanapa/core/themes/dimens_constant.dart';
 import 'package:makanapa/core/widgets/gradient_text.dart';
 import 'package:makanapa/core/widgets/state/error_state_widget.dart';
 import 'package:makanapa/core/widgets/state/loading_state_widget.dart';
+import 'package:makanapa/core/widgets/state/screen_content.dart';
 import 'package:makanapa/features/home/domain/models/weekly_plan.dart';
 import 'package:makanapa/features/home/presentation/plan/controllers/plan_controller.dart';
 import 'package:makanapa/features/home/presentation/plan/controllers/state/plan_event_state.dart';
@@ -25,7 +26,9 @@ class PlanScreen extends HookConsumerWidget {
     );
 
     useEffect(() {
-      ref.read(planControllerProvider.notifier).loadData(groupId);
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        ref.read(planControllerProvider.notifier).loadData(groupId);
+      });
       return null;
     }, const []);
 
@@ -50,47 +53,37 @@ class PlanScreen extends HookConsumerWidget {
       return sub.cancel; // Dispose subscription
     }, const []);
 
-    Widget getWidgetBody(DataState<WeeklyPlan> state) {
-      if (state is Loading) {
-        return const LoadingStateWidget();
-      }
-
-      if (state is Error) {
-        final messages = (state as Error).message;
-        return SliverToBoxAdapter(
-          child: ErrorStateWidget(
-            title: "Ada Masalah",
-            subtitle: messages,
-            buttonText: "Coba Lagi",
-            onButtonPressed: () {
-              ref.read(planControllerProvider.notifier).loadData(groupId);
-            },
-          ),
-        );
-      }
-
-      if (state is Success) {
-        return SliverToBoxAdapter(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("Menu Detail Screen"),
-              Dimens.xxl.space,
-              Icon(Icons.phonelink_erase, size: 80, color: context.secondary),
-              Dimens.xxl.space,
-              GradientText("COMING SOON"),
-              Dimens.xxl.space,
-            ],
-          ),
-        );
-      }
-      return const SliverToBoxAdapter(child: SizedBox());
-    }
-
     return Scaffold(
       appBar: AppBar(title: Text("Detail Menu")),
-      body: SafeArea(child: CustomScrollView(slivers: [getWidgetBody(state)])),
+      body: SafeArea(
+        child: ScreenContent(
+          // To Do Shimmering loading
+          state: state,
+          successWidget: (data) {
+            return SizedBox.expand(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Menu Detail Screen", style: context.titleLarge),
+                  Dimens.xxl.space,
+                  Icon(
+                    Icons.phonelink_erase,
+                    size: 80,
+                    color: context.secondary,
+                  ),
+                  Dimens.xxl.space,
+                  GradientText("COMING SOON"),
+                  Dimens.xxl.space,
+                ],
+              ),
+            );
+          },
+          onRetry: () {
+            ref.read(planControllerProvider.notifier).loadData(groupId);
+          },
+        ),
+      ),
     );
   }
 }
