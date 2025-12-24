a1b7299a6f722b7bfa4dc5e372873e58d21d57fd
FrenskyOey <frenskyoey@gmail.com>
2025-12-24 15:39:38 +0700

    create unit test initial structure

---
+++ makanapa/lib/test/features/shared/main/main_controller_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+//@GenerateMocks([Depedency 1, Depedency 2])
+void main() {
+  group('Controller Test', () {
+    test('Template Test Title', () {});
+  });
+}
