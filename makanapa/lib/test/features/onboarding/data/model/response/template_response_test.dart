cedc9ae749623fff9f963ec786e04044922e6a0d
FrenskyOey <frenskyoey@gmail.com>
2025-12-24 15:41:07 +0700

    update logic

--- makanapa/lib/test/features/onboarding/data/model/response/template_response_test.dart
+++ makanapa/lib/test/features/onboarding/data/model/response/template_response_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 //@GenerateMocks([Depedency 1, Depedency 2])
 void main() {
-  group('Template Test', () {
+  group('Response Model Test', () {
     test('Templat Test Title', () {});
   });
 }
